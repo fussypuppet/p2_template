02b48659004b2c626ebea78c20865ccf705241fe
Eliott R Davidson <fussypuppet@Eliotts-MacBook-Pro.local>
2020-06-29 15:52:50 -0700

    created post method for registration

--- controllers/auth.js
+++ controllers/auth.js
@@ -7,6 +7,35 @@ router.get('/register', function(req,res){
     res.render('auth/register');
 })
 
+router.post('/register', function(req,res){
+    db.user.findOrCreate({
+        where: {
+            email: req.body.email
+        }, defaults {
+            name: req.body.name,
+            password: req.body.password
+        }
+    }).then(function([user,created]){
+        //if user was created
+        if (created){
+            console.log("user created!");
+            res.redirect('/');
+        } else 
+            console.log("User email already exists");
+            req.flash('error', 'Error: email already exists');
+            res.redirect('/auth/register');
+        }.catch(function(err){
+            console.log(`Error found Message: ${err.message}.  Please review ${err}`);
+            req.flash('error', err.message)
+            res.redirect('/auth/register');
+        }
+        //authenticate user and start authorization process
+        // else if user already exists
+            //send error user that email already exists
+            //redirect back to register get route
+    })
+})
+
 router.get('/login', function(req,res){
     res.render("auth/login");
 })
